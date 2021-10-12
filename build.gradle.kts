b11e9e5bb0925936dd5e0b037585c21305a4fd0b
Joshua Soberg <j15soberg@gmail.com>
2021-10-11 21:21:54 -0400

    Add shadowJar gradle plugin for building a runnable jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,32 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.10"
     application
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
-version = "1.0"
+val appVersion = "1.0"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.assertj:assertj-core:3.21.0")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.soberg.netchecker.Main"
+    }
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("NetworkInterfaceChecker")
+    archiveVersion.set(appVersion)
+    archiveClassifier.set("")
+    destinationDirectory.set(file("build/jar/"))
 }
 
 tasks.test {
@@ -25,5 +40,5 @@ tasks.withType<KotlinCompile>() {
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("com.soberg.netchecker.MainKt")
 }
